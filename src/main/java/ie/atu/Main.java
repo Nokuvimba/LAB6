4ccf1e00fef1b5f7784c66c44b4b9f88545a0afa
Nokuvimba <G00423951@atu.ie>
2023-11-06 09:35:56 +0000

    initialised indexArray

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,6 +4,21 @@ package ie.atu;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int[] indexArray;
+
+        indexArray = new int[10];
+        indexArray[0]= 100;
+        indexArray[1]= 200;
+        indexArray[2]= 300;
+        indexArray[3]= 400;
+        indexArray[4]= 500;
+        indexArray[5]= 600;
+        indexArray[6]= 700;
+        indexArray[7]= 800;
+        indexArray[8]= 900;
+        indexArray[9]= 1000;
+
+
 
         }
 
